ea44a39a5407b0c161490eaf550b4ec0f48d2e46
Ramanathan-S-P <137619355+Ramanathan-S-P@users.noreply.github.com>
2024-08-19 15:09:09 +0530

    Auto Commit

---
+++ Array/Can_Place_Flowers.c
@@ -0,0 +1,60 @@
+bool canPlaceFlowers(int* flowerbed, int flowerbedSize, int n) {
+int i=0;
+int count=0;
+    if(flowerbedSize==1){
+if(flowerbed[0]==0){
+    count++;
+    }
+    return count>=n;
+    }
+        
+
+
+   if(flowerbed[0]==0 && flowerbed[1]==0){
+        flowerbed[0]=1;
+        count++;
+        i+=2;
+    }
+ else{
+        i++;
+        }
+    
+    
+while(i<flowerbedSize-1){
+    if(count==n){
+        return 1;
+        }
+     
+
+if(flowerbed[i]==0&&flowerbed[i-1]==0&&flowerbed[i+1]==0){
+
+count++;
+    flowerbed[i]=1;
+
+i+=2;
+
+}
+
+else{
+
+i++;
+
+}
+
+}
+if(  flowerbed[flowerbedSize-1]==0 && flowerbed[flowerbedSize-2]==0){
+    count++;
+    }
+
+    return count>=n;
+    }
+
+
+
+
+
+
+
+
+
+    
