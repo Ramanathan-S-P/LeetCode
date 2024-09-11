952192a4894589f8547983d923fa581164e64c42
Ramanathan-S-P <137619355+Ramanathan-S-P@users.noreply.github.com>
2024-09-11 09:28:45 +0530

    Update Bit_Manipulation

---
+++ Bit_Manipulation/Minimum Bit Flips to Convert Number.c
@@ -0,0 +1,10 @@
+int minBitFlips(int a, int b) {
+   int c=a^b;
+int count=0;
+while(c!=0){
+count+=c&1;
+c>>=1;
+}
+    return count;
+}
+/*rather than introducing  if(c&1)count+=1 do count +=c&1 as this reduces the time utilized  when compared to introducing a conditional branch (i.e if(c&1) ).*/
